188291caba05ce3ef47111b6744f261946662cfb
reethlumindt <reeth@lumindt.com>
2025-07-10 11:03:54 -0700

    hedgehog scripts

--- desorb/desorb_hedgehog.py
+++ desorb/desorb_hedgehog.py
@@ -1,5 +1,6 @@
 from utils.sensors import ADS1115, megaTC
 from utils.kiln import Controller as KilnController
+from utils.alicat_advanced import Controller as FlowController
 import busio
 import board
 import time
@@ -21,7 +22,15 @@ pulse_max=30
 pulse=0
 pulse_flow=0
 
-file='outputs/Molina_B2V1T1_Instrumentation_Activation.csv'
+
+
+FC=FlowController()
+FC.gas='H2'
+FC.ramp=0
+
+ads=ADS1115()
+
+file='outputs/Molina_B2V1T2_Hot_Desorb_2.csv'
 with open(file, 'w', newline='') as f:
     writer=csv.writer(f)
     writer.writerow([
@@ -37,9 +46,17 @@ with open(file, 'w', newline='') as f:
         'T_R1.5_X1 (C)',
         'T_R0_X2 (C)',
         'Vessel Pressure (barG)',
+        'Flow Pressure (barA)',
+        'Flow Temp (C)',
+        'Flow Volumetric Rate (SLPM)',
+        'Flow Mass Rate (g/s)',
+        'Flow Setpoint (g/s)',
+        'Flow Total Mass (g)',
+        'Flow Errors'
     ])
+    FC.totalizer_reset(1)
+    FC.cancel_hold()
     t_start=time.time()
-    t_ref=t_start
     while True:
         try:
             t_now=time.time()
@@ -52,10 +69,8 @@ with open(file, 'w', newline='') as f:
             T_R1_X1 = extraTC.temp(2)[1]
             T_R15_X1 = extraTC.temp(0)[1]
             T_R0_X2 = ads1.temperature(3)
-
-
+            FCdata=FC.poll()
             kiln.update(k_temp)
-            
             writer.writerow([
                 t_now-t_start,
                 kiln.status,
@@ -68,16 +83,30 @@ with open(file, 'w', newline='') as f:
                 T_R1_X1,
                 T_R15_X1,
                 T_R0_X2,
-                v_pres
+                v_pres,
+                FCdata['P'],
+                FCdata['T'],
+                FCdata['V'],
+                FCdata['M'],
+                FCdata['S'],
+                FCdata['A'],
+                FCdata['E']
             ])
             string=(
                 f'{"":-^30}\n'
                 f'Time:         {t_now-t_start:0.2f}\n'
                 f'Kiln Temp:    {k_temp:0.2f} C (CONTROL VARIABLE)\n'
-                f'Surface Temp:  {surf_temp:0.2f} C\n'
+                f'Vessel Temp:  {surf_temp:0.2f} C\n'
                 f'Vessel Pres:  {v_pres:0.2f} barG\n'
                 f'Heat On:      {kiln.status}\n'
                 f'Kiln Paused:  {kiln.pause}\n'
+                f'FC Pres:      {FCdata["P"]:0.2f} barA\n'
+                f'FC Temp:      {FCdata["T"]:0.2f} C\n'
+                f'FC V Flow:    {FCdata["V"]:0.2f} SLPM\n'
+                f'FC M Flow:    {FCdata["M"]:0.6f} g/s\n'
+                f'FC Setpoint:  {FCdata["S"]:0.6f} g/s\n'
+                f'FC Total:     {FCdata["A"]:0.6f} grams\n'
+                f'FC Errors:    {FCdata["E"]}\n'
                 )
             print(string)
             while time.time()-t_now<2:
@@ -85,10 +114,12 @@ with open(file, 'w', newline='') as f:
         except KeyboardInterrupt:
             try:
                 cmd_string=(
-                    f'\n0 -> Kiln off\n'
-                    f'1 -> Kiln on\n'
-                    f'2 -> New kiln setpoint temp\n'
-                    f'3 -> New kiln temp bound\n'
+                    f'\n0 -> Kiln Off\n'
+                    f'1 -> Kiln On\n'
+                    f'2 -> New Kiln Setpoint Temp\n'
+                    f'3 -> New Kiln Temp Bound\n'
+                    f'4 -> New Alicat Setpoint Flow\n'
+                    f'5 -> Reset Alicat Accumulation\n'
                     f'Any other number will continue\n'
                     f'Press ENTER to end script\n'
                 )
@@ -98,11 +129,16 @@ with open(file, 'w', newline='') as f:
                 elif cmd==1:
                     kiln.pause = False
                 elif cmd==2:
-                    new=float(input('New setpoint: '))
+                    new=float(input('New Setpoint: '))
                     kiln.setpoint = new
                 elif cmd==3:
-                    new=float(input('New bound: '))
+                    new=float(input('New Bound: '))
                     kiln.bound = new
+                elif cmd==4:
+                    new=float(input('New Setpoint: '))
+                    FC.setpoint = new
+                elif cmd==5:
+                    FC.totalizer_reset()
                 else:
                     continue
             except:
@@ -111,4 +147,5 @@ with open(file, 'w', newline='') as f:
         except Exception as e:
             print(e)
             break
+    FC.setpoint=0
     kiln.stop()
--- utils/find_devices.py
+++ utils/find_devices.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import smbus
 bus = smbus.SMBus(1)  # Change to 0 if needed
 for addr in range(0x03, 0x78):
@@ -6,3 +7,33 @@ for addr in range(0x03, 0x78):
         print("Found device at: 0x%02X" % addr)
     except OSError:
         pass
+=======
+#!/usr/bin/env python3
+
+import smbus2
+import time
+
+# Use I2C bus 1 (typical on Raspberry Pi)
+bus = smbus2.SMBus(1)
+
+print("Scanning I2C bus for devices...")
+
+# I2C addresses range from 0x03 to 0x77
+found_devices = []
+
+for address in range(0x03, 0x78):
+    try:
+        bus.write_quick(address)
+        found_devices.append(hex(address))
+    except OSError:
+        pass  # No device at this address
+
+if found_devices:
+    print("Found I2C devices at addresses:")
+    for device in found_devices:
+        print(f"  {device}")
+else:
+    print("No I2C devices found.")
+
+bus.close()
+>>>>>>> bce00e0 (hedgehog scripts)
