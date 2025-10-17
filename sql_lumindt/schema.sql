bdb4304f692199677ac3056275c65fabbfd9a796
Kyle Pedersen <kyle@lumindt.com>
2025-10-17 15:04:33 -0700

    rm sql-lumindt dir

--- sql_lumindt/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS test_table (
-    id SERIAL PRIMARY KEY,
-    pi_id TEXT NOT NULL,
-    test_id TEXT,
-    voltage DOUBLE PRECISION,
-    current DOUBLE PRECISION,
-    pressure DOUBLE PRECISION,
-    time TIMESTAMPTZ DEFAULT now()
-);
