ee9d70a2c886ed62534e00a2a4db4fb16de30354
MasonLumindt <mason@lumindt.com>
2026-02-23 21:13:07 +0000

    Added temperature value to output file

--- reactor/record.py
+++ reactor/record.py
@@ -1,4 +1,7 @@
 from .power import MP710256
+from utilities.sensors_v2 import LTC2983
+import busio
+import board
 import os
 import time
 
@@ -6,15 +9,18 @@ import time
 filename = 'reactor/outputs/test_log.csv'
 override = True
 voltage = 3.0
-maximum_current = 1.0
+maximum_current = 11.0
 interval = 1.0  # seconds
 # --------------------------
+
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+LTC=LTC2983(spi)
 
 if os.path.exists(filename) and not override:
     print(f"File '{filename}' already exists. Set 'override = True' to overwrite.")
 else:
     with open(filename, 'w') as f:
-        f.write('Time,Voltage (V),Current (A)\n')
+        f.write('Time,Voltage (V),Current (A), Kiln Temp (C)\n')
         psu = MP710256(port = '/dev/ttyACM0', id = 1)
         psu.VSET = voltage
         psu.ISET = maximum_current
@@ -26,10 +32,11 @@ else:
                 elapsed_time = cycle_start - start_time
                 voltage_reading = psu.VOUT
                 current_reading = psu.IOUT
+                kiln_temp = LTC.temp(1)
                 status = psu.STATUS
                 print_status = {key: status[key] for key in ['CV MODE', 'OUTPUT']}
-                print(f"----------\nTime: {elapsed_time:.3f}s\nVoltage: {voltage_reading:.3f}V\nCurrent: {current_reading:.3f}A\nStatus: {print_status}")
-                f.write(f'{elapsed_time:.3f},{voltage_reading:.3f},{current_reading:.3f}\n')
+                print(f"----------\nTime: {elapsed_time:.3f}s\nVoltage: {voltage_reading:.3f}V\nCurrent: {current_reading:.3f}A\nKiln Temp: {kiln_temp:.3f}C\nStatus: {print_status}")
+                f.write(f'{elapsed_time:.3f},{voltage_reading:.3f},{current_reading:.3f},{kiln_temp:.3f}\n')
                 f.flush()  # Ensure data is written to file immediately
                 while time.time() - cycle_start < interval:
                     # time.sleep(0.001)  # Small sleep to avoid tight loop
